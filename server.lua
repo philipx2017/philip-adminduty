2ed85c3c625d9fa6befc8c4319a6ce58afa7797f
philipx2017 <102838962+philipx2017@users.noreply.github.com>
2022-04-02 03:08:55 +0200

    Add files via upload

--- server.lua
+++ server.lua
@@ -9,11 +9,11 @@ QBCore.Commands.Add('adminduty', 'Adminduty', {}, false, function(source, args)
     QBCore.Functions.ToggleOptin(src)
     if QBCore.Functions.IsOptin(src) then
         TriggerClientEvent('philip-adminduty:AdminDutyOn', src)
-        TriggerClientEvent('QBCore:Notify', src, 'Adminszolgálatba léptél!', 'success')
-        TriggerClientEvent('chat:addMessage', -1, { args = { 'RENDSZER', (GetPlayerName(source) .. " adminszolgálatba lépett!") }, color = { 147, 196, 109 } })
+        TriggerClientEvent('QBCore:Notify', src, 'You are in duty!', 'success')
+        TriggerClientEvent('chat:addMessage', -1, { args = { 'SYSTEM', (GetPlayerName(source) .. " is in duty!") }, color = { 147, 196, 109 } })
     else
         TriggerClientEvent('philip-adminduty:AdminDutyOff', src)
-        TriggerClientEvent('QBCore:Notify', src, 'Kiléptél adminszolgálatból!', 'error')
-        TriggerClientEvent('chat:addMessage', -1, { args = { 'RENDSZER', (GetPlayerName(source) .. " kilépett az adminszolgálatból!") }, color = { 147, 196, 109 } })
+        TriggerClientEvent('QBCore:Notify', src, 'You are no longer in duty!', 'error')
+        TriggerClientEvent('chat:addMessage', -1, { args = { 'SYSTEM', (GetPlayerName(source) .. " has left the duty!") }, color = { 147, 196, 109 } })
     end
 end, 'admin')
